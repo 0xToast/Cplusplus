bd2c02346806ea4192b7d5646331303f044c8a45
0xToast <58208364+0xToast@users.noreply.github.com>
2021-10-24 19:42:30 +0200

    Create Declaring_and_initialising+vectors.cpp

---
+++ Udemy/Section 7/Declaring_and_initialising+vectors.cpp
@@ -0,0 +1,15 @@
+//This section goes over vectors in C++
+
+#include <iostream>
+#include <vector> //Must include the library to use vectors 
+
+using namespace std;
+
+int main() {
+
+    // Initialising vectors
+    vector <char> varChar {'a','e','i','o','u'}; //Self explanitory
+    vector <int> varInt {}; //Self explanitory
+    vector <double> varDouble (365, 80.0); // Initialises 365 doubles at the value of 80.00
+    
+}
