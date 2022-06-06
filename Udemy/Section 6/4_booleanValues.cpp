f8264dfc2cdf3f8f0062eab9895b48cb5c3331ff
0xToast <58208364+0xToast@users.noreply.github.com>
2022-06-06 19:43:44 +0200

    Create 4_booleanValues.cpp

---
+++ Udemy/Section 6/4_booleanValues.cpp
@@ -0,0 +1,12 @@
+//Section 6 Boolean Values
+
+#include <iostream>
+using namespace std;
+
+int main () { 
+    
+    bool gameOver {false};
+    cout << "The value of gameOver is " << gameOver << endl; // 0 = false : 1 = true 
+
+    return 0;   
+}
