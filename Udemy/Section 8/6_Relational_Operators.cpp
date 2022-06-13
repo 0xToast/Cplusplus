c98275d4097ae2e1382f54a44b668733900f8ba0
0xToast <58208364+0xToast@users.noreply.github.com>
2022-06-13 19:00:15 +0200

    Update 6_Relational_Operators.cpp

--- Udemy/Section 8/6_Relational_Operators.cpp
+++ Udemy/Section 8/6_Relational_Operators.cpp
@@ -1,22 +1,21 @@
-// Relational Operators
-
+//Section 8 - Relational Operators
 #include <iostream>
-
-using namespace std;
 
 int main() {
 
     int num1{},num2{};
 
-    cout << boolalpha; // will display true/false instead of 1 or 0 for boolean results.
-    cout << "Enter 2 integers seperated by a space:";
-    cin >> num1 >> num2;
+    //Will display true/false instead of 1 or 0 for boolean results.
+    std::cout << std::boolalpha; 
+
+    std::cout << "Enter 2 integers seperated by a space:";
+    std::cin >> num1 >> num2;
     
-    cout << num1 << " > " << num2 << " : " << (num1 > num2) << endl; //false
-    cout << num1 << " >= " << num2 << " : " << (num1 >= num2) << endl; //false 
-    cout << num1 << " < " << num2 << " : " << (num1 < num2) << endl; //true
-    cout << num1 << " <= " << num2 << " : " << (num1 <= num2) << endl; //true
+    std::cout << num1 << " > " << num2 << " : " << (num1 > num2) << std::endl; //true or false depending on user input
+    std::cout << num1 << " >= " << num2 << " : " << (num1 >= num2) << std::endl; //true or false depending on user input 
+    std::cout << num1 << " < " << num2 << " : " << (num1 < num2) << std::endl; //true or false depending on user input
+    std::cout << num1 << " <= " << num2 << " : " << (num1 <= num2) << std::endl; //true or false depending on user input
     
     return 0;
-  
+
 }
