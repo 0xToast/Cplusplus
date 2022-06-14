444edc8d8f47ee30dda3636587879e6d479c4087
0xToast <58208364+0xToast@users.noreply.github.com>
2022-06-14 20:52:53 +0200

    Update 4_mixedExpressionsAndConversions.cpp

--- Udemy/Section 8/4_mixedExpressionsAndConversions.cpp
+++ Udemy/Section 8/4_mixedExpressionsAndConversions.cpp
@@ -1,4 +1,4 @@
-//Section 8 - Mixed type expressions 
+//Section 8 - Mixed type expressions and casting 
 
 #include <iostream>
 
@@ -19,17 +19,17 @@ int main() {
     std::cout << "average = "<< average << std::endl; //Only displays 12
     std::cout << std::endl;
 
-    //We can use casting to convert totalAmount to the double type to achieve our desired result.
+    // We can use casting to convert totalAmount to the double type to achieve our desired result.
     average = static_cast<double>(totalAmount) / totalNumber;
     std::cout << "After type casting: " << std::endl;
     std::cout << "average = " << average << std::endl; //Displays 12.5
     std::cout << std::endl;
 
-    //In older C++ code Casting looked like this
+    // In older C++ code Casting looked like this
     average = (double)totalAmount / totalNumber; // We do not use this way of casting in modern C++
 
-    //To understand this concept a bit better I looked at more examples
-    //Got the following example off: https://www.ibm.com/docs/en/zos/2.2.0?topic=expressions-static-cast-operator-c-only
+    // To understand this concept a bit better I looked at more examples
+    // Got the following example off: https://www.ibm.com/docs/en/zos/2.2.0?topic=expressions-static-cast-operator-c-only
 
     int j = 41;
     int v = 4;
