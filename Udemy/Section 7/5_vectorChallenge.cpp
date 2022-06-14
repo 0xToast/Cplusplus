11754ddba1fb73750a0fbe5b59605385d8a7f1d7
0xToast <58208364+0xToast@users.noreply.github.com>
2022-06-14 20:44:38 +0200

    Update 5_vectorChallenge.cpp

--- Udemy/Section 7/5_vectorChallenge.cpp
+++ Udemy/Section 7/5_vectorChallenge.cpp
@@ -3,20 +3,20 @@
 #include <iostream>
 #include <vector> 
 
-//Note: From this point I have stopped using namespaces to make my code more readable
-//Watch this video for more info: https://www.youtube.com/watch?v=4NYC-VU-svE
+// Note: From this point I have stopped using namespaces to make my code more readable
+// Watch this video for more info: https://www.youtube.com/watch?v=4NYC-VU-svE
 
 int main() {
 
-    //Declare 2 empty vectors of integers named vector 1 and 2, respectively
+    // Declare 2 empty vectors of integers named vector 1 and 2, respectively
     std::vector <int> vector1;
     std::vector <int> vector2;
 
-    //Add 10 and 20 to vector1 dynamically using push_back
+    // Add 10 and 20 to vector1 dynamically using push_back
     vector1.push_back(10);
     vector1.push_back(20);
 
-    //Display the elements in vector1 using the at() method as well as its size using the size() method
+    // Display the elements in vector1 using the at() method as well as its size using the size() method
     std::cout << "Vector1 values are as follows:" << std::endl;
     std::cout << vector1.at(0) << std::endl; //10
     std::cout << vector1.at(1) << std::endl; //20
@@ -24,11 +24,11 @@ int main() {
    
     std::cout <<std::endl;
 
-    //Add 100 and 200 to vector2 dynamically using push_back
+    // Add 100 and 200 to vector2 dynamically using push_back
     vector2.push_back(100);
     vector2.push_back(200);
 
-    //Display the elements in vector2 using the at() method as well as its size using the size() method
+    // Display the elements in vector2 using the at() method as well as its size using the size() method
     std::cout << "Vector2 values are as follows:" << std::endl; 
     std::cout << vector2.at(0) << std::endl; //100
     std::cout << vector2.at(1) << std::endl; //200
@@ -36,26 +36,26 @@ int main() {
 
     std::cout <<std::endl;
 
-    //Declare an empty 2D vector called vector_2d
-    //Add vector1 to vector2d dynamically using pushback
-    //Add vector2 to vector2d dynamically using pushback
+    // Declare an empty 2D vector called vector_2d
+    // Add vector1 to vector2d dynamically using pushback
+    // Add vector2 to vector2d dynamically using pushback
     std::vector <std::vector<int>> vector2d;
     vector2d.push_back(vector1);
     vector2d.push_back(vector2);
 
-    //Display the all of the elements of vector2d using the at() method
+    // Display the all of the elements of vector2d using the at() method
     std::cout << "Elements of the 2D Vector are as follows:  " << std::endl;
     std::cout << "=========================================" << std::endl;
     std::cout << "Elements of vector1 is: " << vector2d.at(0).at(0) << " " << vector2d.at(0).at(1) << std::endl; //10 20
     std::cout << "Elements of vector2 is: "<< vector2d.at(1).at(0) << " " << vector2d.at(1).at(1) << std::endl; //100 200
 
-    //Change vector1.at(0) to 1000
+    // Change vector1.at(0) to 1000
     std::cout << "\n[!] Changing vector1's 1st element to 1000..." << std::endl;
     vector1.at(0) = 1000;
 
     std::cout <<std::endl; 
 
-    //Display the all of the elements again of vector2d using the at() method
+    // Display the all of the elements again of vector2d using the at() method
     std::cout << "Elements of the 2D Vector are as follows:  " << std::endl;
     std::cout << "=========================================" << std::endl;
     std::cout << "Elements of vector1 is: " << vector2d.at(0).at(0) << " " << vector2d.at(0).at(1) << std::endl; //10 20 -- 1st value did not change to 1000.
@@ -63,7 +63,7 @@ int main() {
    
     std::cout <<std::endl;
 
-    //Display elements of vector1 again.
+    // Display elements of vector1 again.
     std::cout << "Elements of vector1:  " << std::endl; 
     std::cout << vector1.at(0) << std::endl; //1000
     std::cout << vector1.at(1) << std::endl; //20
