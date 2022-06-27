2b86c0e498c0b63828ad428be0f86ecea7145f7a
0xToast <58208364+0xToast@users.noreply.github.com>
2022-06-27 13:59:45 +0200

    Update subsitutionCipherChallenge.cpp

--- Udemy/Section 10/subsitutionCipherChallenge.cpp
+++ Udemy/Section 10/subsitutionCipherChallenge.cpp
@@ -1,5 +1,12 @@
 // Section 10 - Challenge - Substitution Cipher
 
+/* 
+
+My challange in this section was to take a sentence/string inputted by the user and apply a cipher to the text.
+
+I also had to provide funtionality that decoded the text back to it's orgininal state.
+
+*/
 
 
 #include <iostream>
