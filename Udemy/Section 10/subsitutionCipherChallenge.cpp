6c9bb6a7b5350adec0c34871388f2befacad7088
0xToast <58208364+0xToast@users.noreply.github.com>
2022-06-27 13:55:57 +0200

    Create subsitutionCipherChallenge.cpp

---
+++ Udemy/Section 10/subsitutionCipherChallenge.cpp
@@ -0,0 +1,54 @@
+// Section 10 - Challenge - Substitution Cipher
+
+
+
+#include <iostream>
+#include <string> 
+
+int main() {
+
+    // Cipher Key
+    std::string alphabet ("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"); 
+    std::string key      ("QwErTyUiOpAsDfGhJkLzXcVbNmqWeRtYuIoPaSdFgHjKlZxCvBnM");
+
+    // Encode Function
+    std::cout << "\nEncode Function: ";
+    std::cout << "\n===========================";
+
+    std::string message {};
+    std::cout << "\n\nPlease enter your secret message: ";
+    std::getline(std::cin, message); 
+
+        for (size_t i {0}; i < message.size() ; ++i)
+            if (isalpha(message.at(i))){
+
+                size_t position {0};
+                position = alphabet.find(message.at(i)); // 
+
+                std::cout << key.at(position);
+            } else {
+                std::cout << message.at(i);
+            }
+    
+    // Decode Function
+    std::cout << "\n\nDecode Function: ";
+    std::cout << "\n===========================";
+    
+    std::string encodedMessage {};
+
+    std::cout << "\n\nPlease enter encoded message: ";
+    std::getline(std::cin, encodedMessage); 
+
+        for (size_t i {0}; i < encodedMessage.size() ; ++i)
+            if (isalpha(encodedMessage.at(i))){
+                                
+            size_t position {0};
+            position = key.find(encodedMessage.at(i));
+
+            std::cout << alphabet.at(position);    
+            } else {
+                std::cout << encodedMessage.at(i);
+            }   
+                    
+    return 0;   
+}
