3656c3be4a8bdd96bb61fbfe65bef820df4110ab
0xToast <58208364+0xToast@users.noreply.github.com>
2022-06-28 10:25:06 +0200

    Create whatIsAFunctionCStdLib.cpp

---
+++ Udemy/Section 11/whatIsAFunctionCStdLib.cpp
@@ -0,0 +1,31 @@
+// Section 11 - What is a function? - Random Number Generator
+
+#include <iostream>
+#include <cstdlib> // Required for rand() function
+#include <ctime> // required for time() function
+
+int main() {
+
+    int randomNumber {};
+    size_t count {10}; // Number of random numbers to generate
+    const int min {1}; // lowerbound
+    const int max{6}; // upper bound
+
+    std::cout 
+        << "RAND_MAX on my system is: " << RAND_MAX << "\n";
+
+    // Seed the random number generator
+    // If you dont seed the generator you will get the same sequnce of randoms numbers every run
+    srand(time(nullptr));
+
+    for (size_t i {1}; i <= count; ++i) {
+        randomNumber = rand() % max + min;
+        std::cout
+            << randomNumber << "\n";
+    }
+
+    std::cout
+        << "\n";
+
+        return 0;
+}
