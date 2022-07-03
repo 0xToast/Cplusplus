d0c56ac2a5a16a9dc3aff89486a773d733d4753c
0xToast <58208364+0xToast@users.noreply.github.com>
2022-07-03 19:55:41 +0200

    Create overloadingFunctionsExercise.cpp

---
+++ Udemy/Section 11/overloadingFunctionsExercise.cpp
@@ -0,0 +1,30 @@
+// Section 11 - Overloading Functions
+
+#include <iostream>
+#include <cmath>
+
+// Function Prototypes
+double calculateArea (double);
+double calculateArea (double, double);
+
+// Function Definitions
+
+double calculateArea (double x){                // Calculate area of square
+    std::cout
+        << "The area of the square is: ";
+    return pow(x, 2);
+}
+
+double calculateArea (double x, double y){      // Calculate area of rectangle
+    std::cout
+        << "The area of the rectangle is: ";
+    return x * y;
+}
+
+int main (){
+    
+  std::cout
+        << calculateArea(27, 45);
+    
+  return 0;
+}
